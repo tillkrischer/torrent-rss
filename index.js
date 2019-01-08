52c74e9d1d0fd4854a59f2092a44520098480a50
Till Krischer <till@krischer.io>
2019-01-08 23:03:50 +0100

    use ip

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const fs = require("fs");
+const ip = require("ip");
 const RSS = require("rss");
 const express = require("express");
 const app = express();
 
-const folder = process.env.RSS_FOLDER;
+const folder = process.env.RSS_FOLDER || "/";
 
 app.get("/", function(req, res) {
   let feed = new RSS();
   fs.readdirSync(folder).map(file =>
     feed.item({
       title: file,
-      url: "http://" + req.hostname + ":3000/torrents/" + file
+      //      url: "http://" + req.hostname + ":3000/torrents/" + file
+      url: "http://" + ip.address() + ":3000/torrents/" + file
     })
   );
   const xml = feed.xml();
